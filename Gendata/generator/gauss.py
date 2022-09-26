f8c5c7c8c18f6b1e8e87c9d0ec37e4b0c599b386
Anders <ahll19@student.aau.dk>
2022-09-26 14:17:57 +0200

    Fik lavet pakken færdig
    Nu er der en pakke til at lave data med, og der er et tilhørende dokument som viser det vigtige funktionalitet i de pakker

--- Gendata/generator/gauss.py
+++ Gendata/generator/gauss.py
@@ -1,36 +1,32 @@
+import enum
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal as mn
-import pandas as pd
-import pandas_datareader as pdr
-import statsmodels as sm
-from scipy.interpolate import interp1d
-from statsmodels.tsa.api import VAR
 
 
 def gen_MI(n, var_x, var_y, dim=2):
+    """ Generates a set of random points according to <statistical inference
+    of information in networks> on page 51. The third variable, Z, is 
+    ignored as this is only used to create training data for MI, not CMI.
+
+    Args:
+        n (int): number of samples to draw
+        var_x (float): Variance to multiply the identity with
+        var_y (float): Variance to multiply the identity with
+        dim (int, optional): Dimension of the samples to draw. Defaults to 2.
+
+    Returns:
+        _type_: _description_
     """
-    Generates a set of random points according to <statistical inference of information in networks>
-    on page 51. The third variable, Z, is ignored as this is only used to create training data
-    for MI, not CMI.
+    I = np.identity(dim)
+    zero = np.zeros(dim)
+    
+    x_drawn = mn.rvs(mean=zero, cov=var_x*I, size=n)
+    y_drawn = np.zeros(x_drawn.shape)
 
-    The covariance matrices are both a scaled identity matrix.
-
-    :param n: Number of points realized
-    :param var_x: Variance of X
-    :param var_y: Variance of Y
-    :param dim: Dimension of the variables X and Y
-    :return:
-    :returns values: a realization of the process described in the book. Dimensions are [n, X/Y, dim].
-                     e.g. [20, 0, 2] gives the second element the 20th realization of X.
-    :returns mi: The mutual information: I(X; Y)
-    """
-    values = np.zeros((n, 2, dim))  # init
-    values[:, 0, :] = mn.rvs(mean=np.zeros(dim), cov=var_x * np.identity(dim), size=(n, dim)).reshape((n, dim))
-
-    for i, x in enumerate(values[:, 0, :]):
-        values[i, 1, :] = mn.rvs(mean=x, cov=var_y * np.identity(dim), size=1)
-
+    for i, x in enumerate(x_drawn):
+        draw = mn.rvs(mean=x, cov=var_y*I)
+        y_drawn[i] = draw
+    
     mi = dim * np.log2(1 + var_x / var_y) / 2
 
-    return values, mi
+    return x_drawn, y_drawn, mi
--- Gendata/generator/varp.py
+++ Gendata/generator/varp.py
@@ -1,15 +1,10 @@
-# Imports
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import multivariate_normal as mn
-import pandas as pd
 import pandas_datareader as pdr
-import statsmodels as sm
 from scipy.interpolate import interp1d
 from statsmodels.tsa.api import VAR
 
 
-def MI(idx1, idx2, cov):
+def calculate_MI(idx1, idx2, cov):
     """Calculates the mutual information between the marginals of a multivariate gaussian distribution: I(X1; X2)
 
     Args:
@@ -23,10 +18,77 @@ def MI(idx1, idx2, cov):
     top = cov[idx1, idx1] * cov[idx2, idx2]
     bot = top - 2*cov[idx1, idx2]
 
-    return .5 * np.log2(top / bot)
+    div = top / bot
+
+    if div > 0:
+        return .5 * np.log2(div)
+    
+    return 0
 
 
-def simulate_AR(coefficients, noise_cov, num_steps, x0=None):
+def get_data_list():
+    data = __get_fred_data()
+    inter_data = __interpolate_data(data)
+
+    return inter_data
+
+
+def create_coefficients(data_list, series_list, model_order):
+    """Creates the coefficients needed to simulate the data, using statsmodels package. 
+
+    Args:
+        data_list (List of arrays): List of time-series data returned by the interpolation function.
+        series_list (List of int): Indexes (0, ..., 11) which decides what data to use in the model. Len(series_list) = k -> model of dimension k
+        model_order (Int): Order of the model. Must be int >= 1
+
+    Returns:
+        Tuple (array, array): Results from VAR.fit(model_order)
+            array: coefficient matrices. Size is [p, k, k] where p is the model order, and k is the dimension of the model
+            array: covariance matrix. Size is [k, k]
+    """
+    new_data = []
+
+    for idx in series_list:
+        new_data.append(data_list[idx])
+    
+    exog_var = np.array(new_data)
+
+    model = VAR(exog_var.T)
+    result = model.fit(model_order)
+
+    return result.coefs, result.sigma_u
+
+
+def save_load_coefs(name, coefficients=None, covariance=None, save=False):
+    """Save or Load coeffiecients from the AR(p) model
+
+    Args:
+        name (str): name under which the coefficients should be save. using name='abc' saves or loads files 'abc_coefs.npy' and 'abc_cov.npy'
+        coefficients (array): array of the coefficients returned from create_coefficients()
+        covariance (array): array of the covariance returned from create_coefficients()
+        save (bool, optional): If set to True the function saves the data specified in coefficients and covariance. If False the functions loads files specified by name. Defaults to False.
+
+    Returns:
+        Tuple (array, array | None): If save is false the function returns None
+            array: Coefficient matrix loaded by name
+            array: Covariance matrix loaded by name
+    """
+    coef_str = name + "_coefs.npy"
+    cov_str = name + "_cov.npy"
+
+    if save:
+        np.save(coef_str, coefficients)
+        np.save(cov_str, covariance)
+
+        return None
+    
+    coefs = np.load(coef_str)
+    cov = np.load(cov_str)
+
+    return coefs, cov
+
+
+def simulate_VAR(coefficients, noise_cov, num_steps, x0=None):
     """Simulate the AR(p) process, with iid zero-mean gaussian additive noise.
 
     Args:
@@ -36,7 +98,13 @@ def simulate_AR(coefficients, noise_cov, num_steps, x0=None):
         x0 (1D float array, optional): The starting value of the process. If specified the derivations of the MI don't fit as of the end of september 2022. Defaults to None.
     
     Returns:
-        array float: Returns the resulting array of the realization of the process
+        array float: The resulting array of the realization of the process
+                    (note there will be num_steps+1 entries in this, the first being x0)
+        OR
+        tuple (array float, array float):
+            array float: The resulting array of the realization of the process
+                         (note there will be num_steps+1 entries in this, the first being x0)
+            array float: The resulting covariance matrix
     """
     # Setup
     p = len(coefficients)
@@ -44,9 +112,13 @@ def simulate_AR(coefficients, noise_cov, num_steps, x0=None):
     results = np.zeros((num_steps + 1, dim))
 
     # initialize the initial value
+    calc_cov = False
+    
     if x0 is None:
-        x0 = np.zeros(p)
-    results[0] = x0
+        calc_cov = True
+        _x0 = np.zeros(p)
+
+    results[0] = _x0
 
     # simulate the process
     for i in range(1, num_steps + 1):
@@ -63,10 +135,34 @@ def simulate_AR(coefficients, noise_cov, num_steps, x0=None):
         iter_sum += np.random.multivariate_normal(mean=np.zeros(dim), cov=noise_cov)
         results[i] = iter_sum
     
-    return results
-    
+    if x0 is not None:
+        return results
 
-def get_fred_data():
+
+    # Create the vector holding variances, along with a list to hold previous variances needed for the calculation
+    variances = np.zeros((num_steps + 1, dim, dim))
+    p_prev_var = [np.zeros((dim, dim)) for i in range(p)]
+
+    # Initialize the vector holding all variances and the list to calculate new var
+    variances[0] = noise_cov
+    p_prev_var[0] = noise_cov
+
+    for i in range(1, num_steps + 1):
+        # Calculate new variance
+        new_var_list = [__ai(old_var, coefficients[i]) for i, old_var in enumerate(p_prev_var)]
+        new_var = sum(new_var_list) + noise_cov
+
+        # add new variace to p_prev_var list and remove oldest entry
+        new_var_list.insert(0, new_var)
+        del new_var_list[-1]
+
+        # Add the new variance calculated to the vector holding all variances
+        variances[i] = new_var
+    
+    return results, variances[-1]
+
+
+def __get_fred_data():
     """Pulls data from FRED
 
     Returns:
@@ -105,7 +201,7 @@ def get_fred_data():
     return arr_dat
 
 
-def interpolate_data(data_list, recalled=False):
+def __interpolate_data(data_list, recalled=False):
     """Interpolates the data loaded by the function get_fred_data(). Should be able to be generalized if need be.
 
     Args:
@@ -153,7 +249,7 @@ def interpolate_data(data_list, recalled=False):
         return interpolated_data
     
     if not recalled:  # something went wrong in the first case, we try again
-        interpolate_data(data_list, recalled=True)
+        __interpolate_data(data_list, recalled=True)
     
     # We tried twice, and didn't get the data to interpolate
     raise Exception("Data would not interpolate in two tries in function interpolate_data()")
@@ -161,57 +257,12 @@ def interpolate_data(data_list, recalled=False):
     return interpolated_data
 
 
-def create_coefficients(data_list, series_list, model_order):
-    """Creates the coefficients needed to simulate the data, using statsmodels package. 
+def __ai(var, Ai):
+        """
+        Useful function for calculating the running variance at time step
 
-    Args:
-        data_list (List of arrays): List of time-series data returned by the interpolation function.
-        series_list (List of int): Indexes (0, ..., 11) which decides what data to use in the model. Len(series_list) = k -> model of dimension k
-        model_order (Int): Order of the model. Must be int >= 1
-
-    Returns:
-        Tuple (array, array): Results from VAR.fit(model_order)
-            array: coefficient matrices. Size is [p, k, k] where p is the model order, and k is the dimension of the model
-            array: covariance matrix. Size is [k, k]
-    """
-    new_data = []
-
-    for idx in series_list:
-        new_data.append(data_list[idx])
-    
-    exog_var = np.array(new_data)
-
-    model = VAR(exog_var.T)
-    result = model.fit(model_order)
-
-    return result.coefs, result.sigma_u
-
-
-def save_load_coefs(name, coefficients=None, covariance=None, save=False):
-    """Save or Load coeffiecients from the AR(p) model
-
-    Args:
-        name (str): name under which the coefficients should be save. using name='abc' saves or loads files 'abc_coefs.npy' and 'abc_cov.npy'
-        coefficients (array): array of the coefficients returned from create_coefficients()
-        covariance (array): array of the covariance returned from create_coefficients()
-        save (bool, optional): If set to True the function saves the data specified in coefficients and covariance. If False the functions loads files specified by name. Defaults to False.
-
-    Returns:
-        Tuple (array, array | None): If save is false the function returns None
-            array: Coefficient matrix loaded by name
-            array: Covariance matrix loaded by name
-    """
-    coef_str = name + "_coefs.npy"
-    cov_str = name + "_cov.npy"
-
-    if save:
-        np.save(coefficients, coef_str)
-        np.save(covariance, cov_str)
-
-        return None
-    
-    coefs = np.load(coef_str)
-    cov = np.load(cov_str)
-
-    return coefs, cov
-    
+        :param var: variance of the i-previous time-step
+        :param Ai: coefficient matrix to multiply with i-previous time step
+        :return: the product Ai @ var @ Ai.T
+        """
+        return Ai @ var @ Ai.T
